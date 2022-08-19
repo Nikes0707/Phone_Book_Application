53be0ab4538f0a48e7e8e553ada6e54f4cf06be1
Nikheel <nikheelBodkhe1999@gmail.com>
2022-08-19 15:27:03 +0530

    PBA-4:create service Interface and inplemention for that

---
+++ src/main/java/com/NikheelIT/Phone_Book_Application/Service/PhoneBookServiceIMPL.java
@@ -0,0 +1,13 @@
+package com.NikheelIT.Phone_Book_Application.Service;
+
+import com.NikheelIT.Phone_Book_Application.Model.PhoneBook;
+
+public class PhoneBookServiceIMPL implements phoneBookServiceI {
+
+	@Override
+	public PhoneBook createcontact(PhoneBook phonebook) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/NikheelIT/Phone_Book_Application/Service/phoneBookServiceI.java
@@ -0,0 +1,12 @@
+package com.NikheelIT.Phone_Book_Application.Service;
+
+import com.NikheelIT.Phone_Book_Application.Model.PhoneBook;
+
+public interface phoneBookServiceI {
+	
+	
+	
+	public PhoneBook createcontact (PhoneBook phonebook);
+	
+
+}
